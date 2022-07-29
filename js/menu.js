80729d77f2b34a454ecc9048b6e8fe121f7a8597
Mich47 <mmitya889@gmail.com>
2022-07-29 17:50:09 +0300

    add some menu styles

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,32 @@
-// (() => {
-//   const menuBtnRef = document.querySelector('[data-menu-button]');
-//   const mobileMenuRef = document.querySelector('[data-menu]');
-//   const body = document.querySelector('[data-body]');
-
-//   menuBtnRef.addEventListener('click', () => {
-//     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-//     menuBtnRef.classList.toggle('is-open');
-//     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-//     mobileMenuRef.classList.toggle('is-open');
-//     body.classList.toggle('no-scroll');
-//   });
-// })();
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu-modal]'),
-    body: document.querySelector('[data-body]'),
-  };
+  const menuBtnRef = document.querySelector('[data-menu-open]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const body = document.querySelector('[data-body]');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
-  }
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    body.classList.toggle('no-scroll');
+  });
 })();
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-menu-open]'),
+//     closeModalBtn: document.querySelector('[data-menu-close]'),
+//     modal: document.querySelector('[data-menu-modal]'),
+//     body: document.querySelector('[data-body]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+// })();
